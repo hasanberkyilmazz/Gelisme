1f92bb86d4d93ff2300a67571cbb833942b0a25b
Hasan Berk Yilmaz <hasanberkyilmaz@gmail.com>
2025-04-29 11:09:32 +0300

    11. uygulama tamamlandı

--- 11-progress-bar/app.js
+++ 11-progress-bar/app.js
@@ -6,13 +6,6 @@ const job = document.querySelector('.job')
 
 const testimonials = [
     {
-      name: 'Miyah Myles',
-      position: 'Marketing',
-      photo:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6',
-      text: "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
-    },
-    {
       name: 'June Cha',
       position: 'Software Engineer',
       photo: 'https://randomuser.me/api/portraits/women/44.jpg',
@@ -60,7 +53,7 @@ const testimonials = [
  
    userTxt.innerHTML = text
    username.innerHTML = name
-   userjob.innerHTML = position
+   job.innerHTML = position
    userImg.src = photo
  
    idx++
